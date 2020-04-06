56d17a63aa06011e24d2cb2bad2075c4999d7488
nhanover <nhanover@umich.edu>
2020-04-05 22:55:13 -0400

    added segment analytics

--- client/.next/static/runtime/webpack.js
+++ client/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0e2cff162c302addf951";
+/******/ 	var hotCurrentHash = "569219e08a4ad4dab346";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
